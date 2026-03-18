c5d2c7decd556aff2feb249d3c8250d7c5b3a501
MARTIN PETKOV <martin.mpetkov@outlook.com>
2026-03-18 11:18:09 +0200

    Add project files.

---
+++ HouseRentingSystemApi/Controllers/HouseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystemApi.Controllers
+{
+    [Route("api/[controller")]
+    public class HouseController : ControllerBase
+    {
+        public IActionResult GetAll()
+        {
+          
+        }
+    }
+}
