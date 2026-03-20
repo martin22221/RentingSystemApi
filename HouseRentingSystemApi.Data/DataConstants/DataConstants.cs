dd4d5d5da9baba460a25975a85dec02565b2d29f
MARTIN PETKOV <martin.mpetkov@outlook.com>
2026-03-20 09:26:00 +0200

    add dataconstants

---
+++ HouseRentingSystemApi.Data/DataConstants/DataConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystemApi.Data.DataConstants
+{
+    public class DataConstants
+    {
+        public class House()
+        {
+            public const int TitleMaxLength = 50;
+            public const int AddressMaxLength = 150;
+            public const int DescriptionMaxLength = 500;
+        }
+       
+    }
+}
